cc2d98ed065b6ab61fca13742a036228b014808e
SeongkookKIM <ksk8646@naver.com>
2023-06-08 16:41:40 +0900

    공통모듈/물체마다 모듈 만들기(바닥,기둥,빛)

---
+++ src/Floor.js
@@ -0,0 +1,18 @@
+import { Mesh } from "three";
+import { Stuff } from "./Stuff";
+import { cm1, geo, mat } from "./common";
+
+export class Floor extends Stuff {
+  constructor(info) {
+    super(info);
+
+    this.geometry = geo.floor;
+    this.matrial = mat.floor;
+
+    this.mesh = new Mesh(this.geometry, this.matrial);
+    this.mesh.position.set(this.x, this.y, this.z);
+    // this.mesh.castShadow = true;
+    this.mesh.receiveShadow = true;
+    cm1.scene.add(this.mesh);
+  }
+}
