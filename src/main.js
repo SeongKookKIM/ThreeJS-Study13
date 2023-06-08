973411d400290fd0690c5cf8627be40ee2f05317
SeongkookKIM <ksk8646@naver.com>
2023-06-08 15:40:17 +0900

    spotLight복제후 4개 뿌리기/// 공통되는 것들 common.js에 따로 모듈 파일

--- src/main.js
+++ src/main.js
@@ -1,73 +1,98 @@
-import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import * as THREE from "three";
+import { cm1, cm2 } from "./common";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 // ----- 주제: The Bridge 게임 만들기
 
 // Renderer
-const canvas = document.querySelector('#three-canvas');
+// const canvas = document.querySelector("#three-canvas");
 const renderer = new THREE.WebGLRenderer({
-	canvas,
-	antialias: true
+  canvas: cm1.canvas,
+  antialias: true,
 });
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
-// Scene
-const scene = new THREE.Scene();
+// Scene common.js에서 실행
+cm1.scene.background = new THREE.Color(cm2.backgroundColor);
 
 // Camera
 const camera = new THREE.PerspectiveCamera(
-	75,
-	window.innerWidth / window.innerHeight,
-	0.1,
-	1000
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
 );
 camera.position.y = 1.5;
 camera.position.z = 4;
-scene.add(camera);
+cm1.scene.add(camera);
 
 // Light
-const ambientLight = new THREE.AmbientLight('white', 0.5);
-scene.add(ambientLight);
+const ambientLight = new THREE.AmbientLight(cm2.lightColor, 0.8);
+cm1.scene.add(ambientLight);
 
-const directionalLight = new THREE.DirectionalLight('white', 1);
-directionalLight.position.x = 1;
-directionalLight.position.z = 2;
-scene.add(directionalLight);
+const spotLightDistance = 50;
+const spotLight1 = new THREE.SpotLight(cm2.lightColor, 1);
+spotLight1.castShadow = true;
+const spotLight2 = spotLight1.clone();
+const spotLight3 = spotLight1.clone();
+const spotLight4 = spotLight1.clone();
+spotLight1.position.set(
+  -spotLightDistance,
+  spotLightDistance,
+  spotLightDistance
+);
+spotLight2.position.set(
+  spotLightDistance,
+  spotLightDistance,
+  spotLightDistance
+);
+spotLight3.position.set(
+  -spotLightDistance,
+  spotLightDistance,
+  -spotLightDistance
+);
+spotLight4.position.set(
+  spotLightDistance,
+  spotLightDistance,
+  -spotLightDistance
+);
+cm1.scene.add(spotLight1, spotLight2, spotLight3, spotLight4);
 
 // Controls
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 
-
 // Mesh
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshStandardMaterial({
-	color: 'seagreen'
+  color: "seagreen",
 });
 const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+cm1.scene.add(mesh);
 
 // 그리기
 const clock = new THREE.Clock();
 
 function draw() {
-	const delta = clock.getDelta();
+  const delta = clock.getDelta();
 
-	controls.update();
+  controls.update();
 
-	renderer.render(scene, camera);
-	renderer.setAnimationLoop(draw);
+  renderer.render(cm1.scene, camera);
+  renderer.setAnimationLoop(draw);
 }
 
 function setSize() {
-	camera.aspect = window.innerWidth / window.innerHeight;
-	camera.updateProjectionMatrix();
-	renderer.setSize(window.innerWidth, window.innerHeight);
-	renderer.render(scene, camera);
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.render(cm1.scene, camera);
 }
 
 // 이벤트
-window.addEventListener('resize', setSize);
+window.addEventListener("resize", setSize);
 
 draw();
