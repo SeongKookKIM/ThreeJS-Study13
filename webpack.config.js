7ee3af8f4c6366c84b71893206c54751d5487515
SeongkookKIM <ksk8646@naver.com>
2023-06-12 14:02:32 +0900

    runbuild

--- webpack.config.js
+++ webpack.config.js
@@ -73,9 +73,9 @@ module.exports = {
     new CopyWebpackPlugin({
       patterns: [
         { from: "./src/main.css", to: "./main.css" },
-        // { from: "./src/images", to: "./images" },
         { from: "./src/models", to: "./models" },
         { from: "./src/sounds", to: "./sounds" },
+        // { from: "./src/images", to: "./images" },
       ],
     }),
   ],
